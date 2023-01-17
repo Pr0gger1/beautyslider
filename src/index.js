d9257fedfc0b85697c4ac3ddbc4cba33053aa58d
Pr0gger1 <kraus2202200491@gmail.com>
2023-01-17 14:28:03 +0300

    changed package.json

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import Slider from "./components/sliders/Slider";
 export {default as Slider} from './components/sliders/Slider';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
