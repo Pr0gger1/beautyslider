66537e2fb15c71f5c3ea4a59da4c0a72e35258f9
Pr0gger1 <kraus2202200491@gmail.com>
2023-01-18 14:49:35 +0300

    the library has been rewritten using create-react-library

---
+++ src/Slider.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import style from './slider.module.css';
+
+const Slider = ({ checked = false, toggleState, variant = undefined}) => {
+  const [active, isActive] = useState(checked);
+  const variants = {
+    duo: style.duo__color
+  }
+  const customClasses = variants[variant];
+
+  useEffect(() => {
+    if (toggleState) toggleState(active);
+  }, [active, toggleState]);
+
+  return (
+    <div className={style.slider}>
+      <input type="checkbox" checked={active} onChange={e => isActive(e.target.checked)}/>
+      <div className={`${style.flag} ${customClasses}`} onClick={() => isActive(!active)}></div>
+    </div>
+  )
+}
+
+export default Slider;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-export {default as Slider} from './components/sliders/Slider';
+// import React from 'react'
+import Slider from './Slider'
+// import styles from './styles.module.css'
+
+// export const ExampleComponent = ({ text }) => {
+//   return <div className={styles.test}>Example Component: {text}</div>
+// }
+
+export default Slider
