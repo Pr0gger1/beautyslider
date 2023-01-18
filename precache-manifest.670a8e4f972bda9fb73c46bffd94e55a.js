9f3b007d6b43cfcd9708eca0e40ff4a5ec608b8e
Pr0gger1 <kraus2202200491@gmail.com>
2023-01-18 15:09:29 +0300

    Updates

---
+++ precache-manifest.670a8e4f972bda9fb73c46bffd94e55a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5273518b61785759ba2d9f7151b0779c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "126f367a16f6dd0b1ead",
+    "url": "./static/css/main.f8c990fb.chunk.css"
+  },
+  {
+    "revision": "8b53f6d2a62d0ac411f6",
+    "url": "./static/js/2.1e4fa9fb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1e4fa9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126f367a16f6dd0b1ead",
+    "url": "./static/js/main.55c0170d.chunk.js"
+  },
+  {
+    "revision": "b6c49c3a76b61dedbb30",
+    "url": "./static/js/runtime-main.c04e36ec.js"
+  }
+]);
