4866dafa8166d2a7eec2de9ef7df1faad2beca54
Pr0gger1 <kraus2202200491@gmail.com>
2023-01-23 16:24:06 +0300

    example page has been updated, added new slider themes

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,36 @@
 import 'react-app-polyfill/ie11';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import { Thing } from '../.';
+
 import {Slider} from "../src";
+import './styles.css';
 
 const App = () => {
   return (
-    <div>
-      <Slider variant='duo' />
-    </div>
+    <main>
+        <h1>Slider react component</h1>
+        <section className='slider__section'>
+            <div className='default'>
+                <h3>Default slider</h3>
+                <Slider/>
+                <Slider checked={true}/>
+            </div>
+
+            <div className='duo'>
+                <h3>Duo slider</h3>
+                <Slider variant="duo"/>
+                <Slider variant='duo caption' checked={true}/>
+            </div>
+
+            <div className='dark'>
+                <h3>Dark slider</h3>
+                <Slider variant='dark'/>
+                <Slider variant='dark' checked={true}/>
+            </div>
+
+        </section>
+    </main>
   );
 };
 
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,7 +1,5 @@
 import React, {useState} from 'react';
 import './slider.css';
-// const styles = require('./slider.css');
-// import './slider.css';
 
 interface SliderProps {
     checked?: boolean,
@@ -9,25 +7,38 @@ interface SliderProps {
 }
 const Slider = ({ checked = false, variant = '' }: SliderProps) => {
   const [active, isActive] = useState<boolean>(checked);
+  const styles = variant?.split(' ');
   let variants: { [key: string]: string } = {};
     variants = {
-    duo: "duo_color"
+    duo: 'duo__theme',
+    dark: 'dark__theme'
   };
-  const customClasses = variants[variant];
+    let properties: {[key: string]: string} = {
+        caption: "caption"
+    }
+  // const customClasses = variants[variant];
+    const theme = styles.filter(a => a in variants).map(a => variants[a]);
+    const property = styles.filter(a => a in properties).map(a => properties[a]);
+    console.log(theme)
+    // const customClasses = `${theme} ${property}`;
+    // console.log(customClasses)
 
+    // console.log(customClasses);
   // useEffect(() => {
   //   if (toggleState) toggleState(active);
   // }, [active, toggleState]);
 
   return (
-    <div className='slider'>
+    <div className={`slider${property.length ? ' ' + property : ''}`}>
+        <span id='on__sign'>ON</span>
+        <span id='off__sign'>OFF</span>
       <input
         type='checkbox'
         checked={active}
-        onChange={(e) => isActive(e.target.checked)}
+        onChange={e => isActive(e.target.checked)}
       />
       <div
-        className={`flag ${customClasses}`}
+        className={`flag${theme.length ? ' ' + theme : ''}`}
         onClick={() => isActive(!active)}
       />
     </div>
